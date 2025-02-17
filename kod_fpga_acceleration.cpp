8101d626888c2e13eb86d65044d41118b38718e0
Vladimir <bojicvladimir01@gmail.com>
2025-02-17 13:27:40 +0100

    Dodat kod za fpga akceleraciju

--- kod_fpga_acceleration.cpp
+++ kod_fpga_acceleration.cpp
@@ -6,9 +6,6 @@ using namespace std;
 unsigned char maze[MAX_ROWS][MAX_COL]; 
 int rows, cols;
 
-unsigned int lfsr = 0xACE1u; // Inicijalna vrednost LFSR-a
-unsigned int bit;
-
 void nacrtajLavirint(){
     unsigned int i = 0, j = 0;
     
@@ -30,17 +27,19 @@ void nacrtajLavirint(){
 }
 
 void generateMaze() {
-    int stack[MAX_ROWS * MAX_COL][2];
-    int stackTop = -1;
+    char stackX[MAX_ROWS * MAX_COL];
+    char stackY[MAX_ROWS * MAX_COL];
+    char stackTop = -1;
     
-    int pravac[4][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+    char pravac[4][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
 
-    // Premestena deklaracija promenljivih na pocetak
-    int i = 0, j = 0;
-    int trenutnoX, trenutnoY, trenutniPravac;
-    int novoX, novoY, slucajniPravac;
-    int zamenaPravca[2];
-    
+    char i = 0, j = 0;
+    char trenutnoX, trenutnoY, trenutniPravac;
+    char novoX, novoY, slucajniPravac;
+    char tempPravacX, tempPravacY; // Promenljive za privremeno čuvanje pravaca
+    unsigned int lfsr = 0xACE1u; // Inicijalna vrednost LFSR-a
+    unsigned int bit;
+
     lpocetak: if (i < rows) goto lspoljasnje; else goto ldalje;
     
     lspoljasnje: if (j < cols) {
@@ -55,34 +54,33 @@ void generateMaze() {
 
     ldalje: 
     stackTop++;
-    stack[stackTop][0] = 0;
-    stack[stackTop][1] = 0;
+    stackX[stackTop] = 0; // X koordinata
+    stackY[stackTop] = 0; // Y koordinata
     maze[0][0] = 0;
 
     lstack: if (stackTop >= 0) goto lprocesiraj; else goto lend;
     
     lprocesiraj:
-    trenutnoX = stack[stackTop][0];
-    trenutnoY = stack[stackTop][1];
+    trenutnoX = stackX[stackTop];
+    trenutnoY = stackY[stackTop];
     stackTop--;
 
     trenutniPravac = 0;
 
     lmesanje: if (trenutniPravac < 4) {
-        // Generisanje slučajnog pravca pomoću LFSR-a
         bit = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1;
         lfsr = (lfsr >> 1) | (bit << 15);
         slucajniPravac = lfsr % 4;
 
-        // Zamena pravaca
-        zamenaPravca[0]= pravac[trenutniPravac][0];
-        zamenaPravca[1] = pravac[trenutniPravac][1];
+        
+        tempPravacX = pravac[trenutniPravac][0];
+        tempPravacY = pravac[trenutniPravac][1];
 
         pravac[trenutniPravac][0] = pravac[slucajniPravac][0];
         pravac[trenutniPravac][1] = pravac[slucajniPravac][1];
 
-        pravac[slucajniPravac][0] =  zamenaPravca[0];
-        pravac[slucajniPravac][1] = zamenaPravca[1];
+        pravac[slucajniPravac][0] = tempPravacX;
+        pravac[slucajniPravac][1] = tempPravacY;
 
         trenutniPravac++;
         goto lmesanje;
@@ -98,12 +96,12 @@ void generateMaze() {
         novoY = trenutnoY + 2 * pravac[trenutniPravac][1];
 
         if (novoX >= 0 && novoX < rows && novoY >= 0 && novoY < cols && maze[novoX][novoY] == 1) {
-            maze[novoX - pravac[trenutniPravac][0]][novoY - pravac[trenutniPravac][1]] = 0;
-            maze[novoX][novoY] = 0;
+            maze[novoX - pravac[trenutniPravac][0]][novoY - pravac[trenutniPravac][1]] = 0;//rusimio zid izmedju trenunte i nardne celije
+            maze[novoX][novoY] = 0;//nova koordinata je sada deo laviritna
 
             stackTop++;
-            stack[stackTop][0] = novoX;
-            stack[stackTop][1] = novoY;
+            stackX[stackTop] = novoX;
+            stackY[stackTop] = novoY;
         }
         trenutniPravac++;
         goto lnovekoordinate;
@@ -115,10 +113,10 @@ void generateMaze() {
     nacrtajLavirint();
 }
 
+
 int main() {
-    rows = 25;
-    cols = 25;
+    rows = 33;
+    cols = 33;
     generateMaze();
     return 0;
 }
-
